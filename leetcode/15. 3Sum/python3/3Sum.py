2a04d3132af4fb2b6f24b444b255bcd790f90356
sunpochin <sunpochin@gmail.com>
2021-01-14 14:03:09 +0800

    modifying 15.

--- leetcode/15. 3Sum/python3/3Sum.py
+++ leetcode/15. 3Sum/python3/3Sum.py
@@ -1,38 +1,27 @@
-
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def searchPair(self, nums, target, left, triplets):
-        right = len(nums) - 1
-        while(left < right):
-            curSum = nums[left] + nums[right]
-            if (curSum == target):
-                triplets.append([-target, nums[left], nums[right]] )
-                left += 1
-                right -= 1
-                while left < right and nums[left] == nums[left - 1]:
-                    left += 1  # skip same element to avoid duplicate triplets
-                while left < right and nums[right] == nums[right + 1]:
-                    right -= 1  # skip same element to avoid duplicate triplets
-            elif curSum < target:
-                left += 1
-            else:
-                right -= 1
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        first = None
+        second = None
+        length = 0
+        first = head
+        while(first != None):
+            first = first.next
+            length += 1
+        print('length: ', length)
         
+        first = head
+        for ni in range(length - n - 1):
+            first = first.next
+        print('first: ', first)
+        if (first.next != None):
+            first.next = first.next.next
         
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        print('nums: ', nums)
-        nums.sort()
-        print('nums: ', nums)
-        triplets = []
-        for it in range(len(nums)):
-            # skip same element to avoid duplicate triplets
-            if it > 0 and nums[it] == nums[it-1]:  
-                continue
-#            self.searchPair(it+1)
-            self.searchPair(nums, -nums[it], it+1, triplets)    
-        return triplets            
-        
+        return dummy.next
             
-            
-            
-        
