7faf7029721c815e70d041f8dfdba9e1d944ec34
sunpochin <sunpochin@gmail.com>
2021-01-25 23:00:50 +0800

    adding 62

--- leetcode/62. Unique Paths/python3/solution.py
+++ leetcode/62. Unique Paths/python3/solution.py
@@ -1,3 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
+        fact = math.factorial(m+n-2) / ( math.factorial(m-1) * math.factorial(n-1) )
+        return int(fact)
+    
+
