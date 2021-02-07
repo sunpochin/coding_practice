f38606d499833da12eb9633c78b19966b6fc317e
sunpochin <sunpochin@gmail.com>
2021-02-08 07:31:51 +0800

    adding 191

---
+++ leetcode/191. Number of 1 Bits/python3/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+    # https://leetcode.com/problems/number-of-1-bits/discuss/1044775/Python-n-and-(n-1)-trick-%2B-even-faster-explained
+    # https://leetcode.wang/leetcode-191-Number-of-1-Bits.html
