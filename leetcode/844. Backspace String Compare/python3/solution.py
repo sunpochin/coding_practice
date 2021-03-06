d0385741135b23f853a280ffa56cb0bb6994cf74
sunpochin <sunpochin@gmail.com>
2021-03-07 03:56:54 +0800

    adding 844

--- leetcode/844. Backspace String Compare/python3/solution.py
+++ leetcode/844. Backspace String Compare/python3/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
         stackS = []
         stackT = []
-        print('stackS: ', stackS)
-        print('stackT: ', stackT)
+        # print('stackS: ', stackS)
+        # print('stackT: ', stackT)
 
         for s in S:
 #            print('s: ', s)
@@ -13,7 +13,7 @@ class Solution:
             else:
                 stackS.append(s)
 #            print('stackS: ', stackS)
-        print('stackS: ', stackS)
+#        print('stackS: ', stackS)
         
         for t in T:
 #            print('t: ', t)
@@ -23,14 +23,14 @@ class Solution:
             else:
                 stackT.append(t)
 #            print('stackT: ', stackT)
-        print('stackT: ', stackT)
+#        print('stackT: ', stackT)
         return self.CompareStr(stackS, stackT)
     
     def CompareStr(self, stackS: list, stackT: list) -> bool:
         if len(stackS) != len(stackT):
             return False
         for it in range(len(stackS)):
-            print('stackS[it]: ', stackS[it])
+#            print('stackS[it]: ', stackS[it])
             if stackS[it] != stackT[it]:
                 return False
         # else
