f427cb61dfdc9214e1bc949f285d91c8ec699e1a
sun pochin <sunpochin@gmail.com>
2021-03-16 15:27:13 +0800

    adding 104

---
+++ leetcode/104. Maximum_Depth_of_Binary_Tree/python3/solu.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recur(self, node: TreeNode, curDepth):
+        if (not node):
+            return curDepth
+        curDepth += 1
+        return max(self.recur(node.left, curDepth), 
+                    self.recur(node.right, curDepth) )
+
+    
+    def maxDepth(self, root: TreeNode) -> int:
+        return self.recur(root, 0)
+        
