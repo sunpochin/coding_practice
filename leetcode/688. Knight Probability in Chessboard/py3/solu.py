cd6692812c15be59b2b7ad23e8a13b6738731c64
sun pochin <sunpochin@gmail.com>
2021-03-25 08:27:23 +0800

    adding 688.

--- leetcode/688. Knight Probability in Chessboard/py3/solu.py
+++ leetcode/688. Knight Probability in Chessboard/py3/solu.py
@@ -1,39 +1,49 @@
-class Solution:
-    # brute force
-    dirs = {(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1) }
-    def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
-        print('r: ', r, ', c: ', c)
-        if (r < 0 or c < 0 or r >= N or c >= N):
-            return 0
+# # brute force
+# class Solution:
+#     dirs = {(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1) }
+#     def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
+#         print('r: ', r, ', c: ', c)
+#         if (r < 0 or c < 0 or r >= N or c >= N):
+#             return 0
         
-        if (0 == K):
-            return 1
+#         if (0 == K):
+#             return 1
         
-        res = 0
-        for dir in self.dirs:
-            res += self.knightProbability(N, K - 1, r + dir[0], c + dir[1]) / 8
-            print('res: ', res )
-        print('break:')
-        return res
+#         res = 0
+#         for dir in self.dirs:
+#             res += self.knightProbability(N, K - 1, r + dir[0], c + dir[1]) / 8
+#             print('res: ', res )
+#         print('break:')
+#         return res
 
     
-#     def recurse(self, curPos, dir8):
-# #        dir8 = {(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1) }
-#         if (curPos[0] >=  or curPos[1] >= 0):
-#             return False
-#         for iy in range(dir8):
-#             newPos = (curPos[0] + iy[0], curPos[0] + iy[1])
-#             print('newPos:', newPos)
-            
-            
-# #        newPos = (curPos[0] + dir[0], curPos[0] + dir[1])
-# #        newPos = curPos + dir
-                
-#     def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
-#         # brute force first.
-#         dir8 = {(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1) }
-#         curPos = (r, c)
-#         for it in range(K):
-#             # knight goes from (r, c), to 8 directions.
-#             for dir in dir8:
-#                 curPos = self.recurse(curPos, dir8)
+# Memoizing top down DP solution.
+class Solution:
+    dirs = {(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1) }
+    def recurse(self, N: int, K: int, r: int, c: int, dp: []):
+        if (r < 0 or c < 0 or r >= N or c >= N):
+            return 0
+        if (0 == K):
+            return 1
+#        print('dp[K][r][c]: ', dp[K][r][c])
+        if (0 != dp[K][r][c]):
+            return dp[K][r][c]
+        
+        res = 0
+#        print('self.dirs: ', self.dirs )
+        
+        for dir in self.dirs:
+            res += self.recurse(N, K - 1, r + dir[0], c + dir[1], dp) / 8
+#            print('res: ', res )
+#        print('break:')
+        dp[K][r][c] = res
+        return dp[K][r][c] 
+        
+        
+    def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
+        dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(K + 1)]
+#        dp = [[[0 for x in range(c)] for y in range(r)]]
+#        print('K: ', K, ', r: ', r, ', c: ', c, ', dp: ', dp)
+        
+        return self.recurse(N, K, r, c, dp)
+
