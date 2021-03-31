ef1dc5e9533190b75049d51e506247c8680fb295
sun pochin <sunpochin@gmail.com>
2021-03-31 13:43:16 +0800

    adding 104

--- leetcode/104. Maximum_Depth_of_Binary_Tree/python3/solu.py
+++ leetcode/104. Maximum_Depth_of_Binary_Tree/python3/solu.py
@@ -17,38 +17,3 @@ class Solution:
         return self.recur(root, 0)
         
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        que = []
-        res = []
-        que.append(root)
-        BFlen = 1
-        
-        while(len(que)):
-            node = que.pop(0)
-            res.append(node)
-            if (len(res) < BFlen):
-                # continue
-                print('< que: ', que)
-                
-                if (node.left):
-                    que.append(node.left)
-                if (node.right):
-                    que.append(node.right)
-            else:
-                res.append(que)
-                print('this lvl done, que: ', que)
-                
-            
-            
-        return res
-    
-    
-    
-
