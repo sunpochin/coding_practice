019b694a971022481e1e8f2bdcfbc6643d690acb
sun pochin <sunpochin@gmail.com>
2021-04-01 21:06:59 +0800

    adding 121

--- leetcode/111. Minimum Depth of Binary Tree/py3/solu.py
+++ leetcode/111. Minimum Depth of Binary Tree/py3/solu.py
@@ -4,6 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# Algorithm:
+# 1. pass height into recur, height + 1.
+# 2. if None == node: return height
+# 3. update minimum height.
+
+
 class Solution:
+    def recur(self, node: TreeNode, height: int):
+        if None == node:
+            return height
+        recur(node.left)
+        recur(node.right)
+
     def minDepth(self, root: TreeNode) -> int:
+        self.recur(root, 0)
+        
         
