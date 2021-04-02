6a9385124b3555fc3a329bc4abe9c18add2478b9
sun pochin <sunpochin@gmail.com>
2021-04-03 01:28:58 +0800

    adding 71

---
+++ leetcode/71. Simplify Path/py3/solu.py
@@ -0,0 +1,31 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        parsed = path.split("/")
+        # print('parsed: ', parsed)
+        stack = []
+        for it in parsed:
+            if '' == it:
+                continue
+            if '.' == it:
+                continue
+            
+            if '..' == it:
+                if len(stack) > 0:
+                    stack.pop(len(stack) - 1)
+                elif len(stack) is 0:
+                    continue
+            else:
+                stack.append(it)
+                
+        # print('stack: ', stack)
+        res = ''
+        for it in stack:
+            res = res + '/' + it
+        if '' == res:
+            res = '/'
+            
+        # print('res: ', res)
+        return res
+        
+                
+                
