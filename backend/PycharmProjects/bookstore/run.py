4ca81fbf04ba764ede427e31a54ef2b976ecd912
sun pochin <sunpochin@gmail.com>
2021-04-25 23:29:47 +0800

    course code along update

--- backend/PycharmProjects/bookstore/run.py
+++ backend/PycharmProjects/bookstore/run.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from models.user import User
 
 app = FastAPI()
 
 @app.get("/hello")
 async def hello_world():
     return {"Hello fastapi world!"}
+
+@app.post("/user")
+async def post_user(user: User):
+    return {"request USER:": user}
+
+@app.get("/user")
+async def get_user_validation(password:str):
+    return {"QUERY parameter: ":password}
