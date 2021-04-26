da4bf4b1142627240b9952f150ce1603b1bf6059
sun pochin <sunpochin@gmail.com>
2021-04-26 23:34:00 +0800

    adding author and book models.

---
+++ backend/PycharmProjects/bookstore/models/author.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+# from models.book import Book
+from typing import List
+
+class Author(BaseModel):
+    name: str
+    book: List[str]
+
--- backend/PycharmProjects/bookstore/run.py
+++ backend/PycharmProjects/bookstore/run.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+# from models.author import Author
+# from models.book import Book
 from models.user import User
 
 app = FastAPI()
@@ -13,4 +15,15 @@ async def post_user(user: User):
 
 @app.get("/user")
 async def get_user_validation(password:str):
-    return {"QUERY parameter: ":password}
+    return {"QUERY parameter ": password}
+
+@app.get("/book/{isbn}")
+async def get_book_with_isbn(isbn: str):
+    return {"Query CHANGABLE parameter ": isbn}
+
+
+@app.get("/author/{id}/book")
+async def get_authors_books(id: int, category: str, order: str = "asc"):
+    return {"Both pass and query parameter ": order + category + str(id) }
+
+
