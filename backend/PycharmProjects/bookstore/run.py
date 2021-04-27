70f7e08f8e05d0785e575b851f80f24e6c0e2288
sun pochin <sunpochin@gmail.com>
2021-04-27 23:01:43 +0800

    updating

--- backend/PycharmProjects/bookstore/run.py
+++ backend/PycharmProjects/bookstore/run.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-# from models.author import Author
+from fastapi import FastAPI, Body
+from models.author import Author
 # from models.book import Book
 from models.user import User
 
@@ -21,9 +21,14 @@ async def get_user_validation(password:str):
 async def get_book_with_isbn(isbn: str):
     return {"Query CHANGABLE parameter ": isbn}
 
-
 @app.get("/author/{id}/book")
 async def get_authors_books(id: int, category: str, order: str = "asc"):
     return {"Both pass and query parameter ": order + category + str(id) }
 
+@app.patch("/author/name")
+async def patch_author_name(name: str = Body(..., embed=True) ):
+    return {"name in body": name}
 
+@app.post("/user/author")
+async def post_user_and_author(user: User, author: Author, bookstore_name: str = Body(..., embed=True) ):
+    return {"user": user, "author": author, "bookstore_name": bookstore_name}
