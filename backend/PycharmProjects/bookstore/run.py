54321ea5c69c232764c5d8c083be9e434a84b449
sun pochin <sunpochin@gmail.com>
2021-05-03 22:53:40 +0800

    adding reponse model.

--- backend/PycharmProjects/bookstore/run.py
+++ backend/PycharmProjects/bookstore/run.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Header
 from models.author import Author
-# from models.book import Book
+from models.book import Book
 from models.user import User
 
 app = FastAPI()
@@ -10,16 +10,32 @@ async def hello_world():
     return {"Hello fastapi world!"}
 
 @app.post("/user")
-async def post_user(user: User):
-    return {"request USER:": user}
+async def post_user(user: User, x_custom: str = Header("default header") ):
+    return {"request USER:": user, "request custom header": x_custom}
 
 @app.get("/user")
 async def get_user_validation(password:str):
     return {"QUERY parameter ": password}
 
-@app.get("/book/{isbn}")
+@app.get("/book/{isbn}", response_model=Book, response_model_include=["name"] )
 async def get_book_with_isbn(isbn: str):
-    return {"Query CHANGABLE parameter ": isbn}
+    author_dict = {
+        "name": "author1",
+        "book": ["book1", "book2"]
+    }
+    author1 = Author(**author_dict)
+    book_dict = {
+        "isbn": "isbn1",
+        "name": "book1",
+        "year": 2019,
+        "author": author1
+    }
+    book1 = Book(**book_dict)
+    return book1
+
+# @app.get("/book/{isbn}")
+# async def get_book_with_isbn(isbn: str):
+#     return {"Query CHANGABLE parameter ": isbn}
 
 @app.get("/author/{id}/book")
 async def get_authors_books(id: int, category: str, order: str = "asc"):
