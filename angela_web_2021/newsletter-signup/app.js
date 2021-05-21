b70e36abc9d4a4e0d84285541048c78db191083f
sun pochin <sunpochin@gmail.com>
2021-05-21 16:45:32 +0800

    updating newsletter to heroku

--- angela_web_2021/newsletter-signup/app.js
+++ angela_web_2021/newsletter-signup/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser")
 const express = require("express")
 const request = require("request")
+const https = require("https")
 
 app = express()
 app.use(express.static("public"));
@@ -10,14 +11,64 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html")
 })
 
-app.post("/", function(req, res) {
-  console.log("req.body: ", req.body.toString() )
-  var first = req.body.fName;
-  var last = req.body.lName;
-  var email = req.body.email;
-  console.log("first: ", req.body.fName, req.body.lName, req.body.email)
+app.post("/subscribe_another", function(req, res) {
+  res.redirect("/");
 })
 
-app.listen(3000, function() {
-  console.log("listening on 3000...")
+app.post("/", function(req, res) {
+  console.log("req.body: ", req.body.toString() )
+  const firstname = req.body.fName;
+  const lastname = req.body.lName;
+  const email = req.body.email;
+  console.log("first: ", req.body.fName, req.body.lName, req.body.email)
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname
+        }
+      }
+    ]
+  }
+
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us6.api.mailchimp.com/3.0/lists/61e70f2a45"
+  const options = {
+    method: "POST",
+    auth: "pachinko:4f205585564097b36b114b4e37ef52dd-us6"
+  }
+
+  var responseCode;
+  const request = https.request(url, options, function(response) {
+    console.log("response: ", response.statusCode);
+    if (200 === response.statusCode) {
+      // res.send("Success!")
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      // console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+  // console.log("response 2: ", response.statusCode);
+  // console.log("response 3: ", response.statusCode);
 })
+
+// for heroku
+app.listen(process.env.PORT || 3000, function() {
+  console.log("listening on: ", process.env.PORT)
+})
+
+
+//
+// 4f205585564097b36b114b4e37ef52dd-us6
+
+// 61e70f2a45
