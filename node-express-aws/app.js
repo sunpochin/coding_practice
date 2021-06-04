980d9b7305ac5d4af263cbd6b2eaa0fed1b32c11
sun pochin <sunpochin@gmail.com>
2021-06-04 19:27:34 +0800

    Serve stylesheets statically with nginx.

--- node-express-aws/app.js
+++ node-express-aws/app.js
@@ -1,6 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var hike = require('./routes/hike');
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -8,7 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.get('/hikes', hike.index);
+app.post('/add_hike', hike.add_hike);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
